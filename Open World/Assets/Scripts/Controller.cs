860246d0ced81005a1d0ef7f6df050e39c1bbb37
Andres Pereira <andres2.pereira@live.uwe.ac.uk>
2018-11-19 13:07:39 +0000

    Implemented loading and unloading scenes

---
+++ Open World/Assets/Scripts/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 30.0f;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
---
+++ Open World/Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0f;
+
+	private void Start ()
+    {
+		
+	}
+
+    private void Update()
+    {
+        currentX += (Input.GetAxis("Mouse X") * 2);
+        currentY += (Input.GetAxis("Mouse Y") * 2);
+    }
+
+	private void LateUpdate ()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+	}
+}
