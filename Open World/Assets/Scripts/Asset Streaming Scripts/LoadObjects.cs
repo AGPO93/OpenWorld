f3765a8eb61b4c6dbbc539f4b29bc695948b2bd9
Andres Pereira <andres2.pereira@live.uwe.ac.uk>
2018-11-29 16:04:07 +0000

    Made XML files with all the scene assets

--- Open World/Assets/Scripts/Asset Streaming Scripts/LoadObjects.cs
+++ Open World/Assets/Scripts/Asset Streaming Scripts/LoadObjects.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class LoadObjects : MonoBehaviour
 {
+    #region Area Lists
     [SerializeField]
     private List<GameObject> Area_1 = new List<GameObject>();
     [SerializeField]
-    private List<GameObject> terrain_list = new List<GameObject>();
-    private GameObject objToInstantiate;
+    private List<GameObject> Area_2 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_3 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_4 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_5 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_6 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_7 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_8 = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> Area_9 = new List<GameObject>();
+    #endregion
+
     ObjectContainer xmlContainer = new ObjectContainer();
     ObjectPrefab xmlObject = new ObjectPrefab();
 
     private void Start()
     {
         //WriteXML();
-        xmlContainer = ObjectContainer.Load("Assets/TextFiles/xmlFile.xml"); 
+        xmlContainer = ObjectContainer.Load("Assets/TextFiles/Area9.xml"); 
 
         foreach (ObjectPrefab object_prefab in xmlContainer.objectList)
         {
@@ -25,14 +41,16 @@ public class LoadObjects : MonoBehaviour
 
     private void createObject(string name, Vector3 position, Vector3 scale)
     {
-        objToInstantiate = Instantiate(Resources.Load(name, typeof(GameObject))) as GameObject;
-        objToInstantiate.transform.position = position;
-        objToInstantiate.transform.localScale = scale;
+        GameObject _object;
+
+        _object = Instantiate(Resources.Load(name, typeof(GameObject))) as GameObject;
+        _object.transform.position = position;
+        _object.transform.localScale = scale;
     }
 
     private void WriteXML()
     {
-        foreach (GameObject objectPrefab in Area_1)
+        foreach (GameObject objectPrefab in Area_9)
         {
             xmlObject.name = objectPrefab.name;
             xmlObject.position = objectPrefab.transform.position;
@@ -40,8 +58,8 @@ public class LoadObjects : MonoBehaviour
             xmlObject.scale = objectPrefab.transform.localScale;
 
             xmlContainer.objectList.Add(xmlObject);
-            xmlContainer.Save("Assets/TextFiles/xmlFile.xml");
-            xmlContainer = ObjectContainer.Load("Assets/TextFiles/xmlFile.xml");
+            xmlContainer.Save("Assets/TextFiles/Area9.xml");
+            xmlContainer = ObjectContainer.Load("Assets/TextFiles/Area9.xml");
         }
     }
 }
